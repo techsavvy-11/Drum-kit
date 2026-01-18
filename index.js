27b648339480105f1f48d5da0ee6de4fa046e5be
TechSavvy_11 <117170043+techsavvy-11@users.noreply.github.com>
2026-01-18 13:16:11 +0530

    index.js

--- index.js
+++ index.js
@@ -1,44 +1,53 @@
-
-function outputSound(key){
-    switch (key) {
+function pressedKey(key){
+         switch(key){
             case "w":
-                new Audio("./sounds/tom-1.mp3").play();
+                let tom1 = new Audio('./sounds/tom-1.mp3');
+                tom1.play();
                 break;
-
             case "a":
-                new Audio("./sounds/tom-2.mp3").play();
+                let tom2 = new Audio('./sounds/tom-2.mp3');
+                tom2.play();
                 break;
-
             case "s":
-                new Audio("./sounds/tom-3.mp3").play();
+                let tom3 = new Audio('./sounds/tom-3.mp3');
+                tom3.play();
                 break;
-
             case "d":
-                new Audio("./sounds/tom-4.mp3").play();
+                let tom4 = new Audio('./sounds/tom-4.mp3');
+                tom4.play();
                 break;
-
             case "j":
-                new Audio("./sounds/snare.mp3").play();
-                break;
-
+                let snare = new Audio('./sounds/snare.mp3');
+                snare.play();
+                 break;
             case "k":
-                new Audio("./sounds/crash.mp3").play();
+                let crash = new Audio('./sounds/crash.mp3');
+                crash.play();
                 break;
-                
             case "l":
-                new Audio("./sounds/kick-bass.mp3").play();
+                let kick = new Audio('./sounds/kick-bass.mp3');
+                kick.play();
                 break;
-
             default:
-    }
+                console.log(this.innerHTML);
+        }
 }
 
-for(var i=0; i<document.querySelectorAll("button").length;i++){
-    document.querySelectorAll("button")[i].addEventListener("click", function() {
-        outputSound(this.textContent);
+for(let i=0; i < document.querySelectorAll(".drum").length; ++i){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+        pressedKey(this.innerHTML);
+        buttonAnimation(this.innerHTML);
     });
 }
 
 document.addEventListener("keydown", function(event){
-    outputSound(event.key);
+    pressedKey(event.key);
+    buttonAnimation(event.key);
 });
+
+function buttonAnimation(currentKey){
+    document.querySelector("." + currentKey).classList.add("pressed");
+    setTimeout(function() {
+        document.querySelector("." + currentKey).classList.remove("pressed");
+    }, 100);
+}
